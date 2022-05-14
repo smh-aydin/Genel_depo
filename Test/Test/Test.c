46a106d51626d7a41868c5b97871f133e503fcc8
semih_aydin <smh.aydiin@gmail.com>
2022-05-14 16:03:21 +0300

    C_ile_test_for_döngüsü

--- Test/Test/Test.c
+++ Test/Test/Test.c
@@ -1,10 +1,18 @@
 
 #include <iostream>
+#include <stdio.h>
 
 int main()
 {
     printf("Hello World/n");
-    printf("SemihAydin");
+    printf("SemihAydin/n");
+    printf("AhmetBasli/n");
+
+    for (int i = 10; i < 0; i--)
+    {
+        printf("%d/n",i);
+    }
+
     return 0;
 
 }
